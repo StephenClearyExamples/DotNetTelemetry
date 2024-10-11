862f8e449a4727a2e5f48d752552a4c742091ecd
Stephen Cleary <github.ourteddybear@xoxy.net>
2024-10-11 10:40:39 -0400

    Add mysql db.

--- AppHost1/Program.cs
+++ AppHost1/Program.cs
@@ -7,6 +7,9 @@ builder.AddResource(new ProjectResource("api")).WithAnnotation(new WebApi());
 builder.AddRabbitMQ("queue", builder.AddParameter("rabbitmq-username"), builder.AddParameter("rabbitmq-password"), 5672)
 	.WithManagementPlugin();
 
+builder.AddMySql("db", builder.AddParameter("mysql-password"), 3306)
+	.AddDatabase("work", "work");
+
 builder.AddResource(new ProjectResource("workerservice")).WithAnnotation(new WorkerService());
 
 builder.Build().Run();
